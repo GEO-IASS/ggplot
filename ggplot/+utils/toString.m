77136b211b1e67662070fd425d4abbf7ec3e9a97
Nick Roth <nlr06886@gmail.com>
2014-08-20 22:25:50 -0500

    added to string function

---
+++ ggplot/+utils/toString.m
@@ -0,0 +1,35 @@
+function [ str_items ] = toString( items, varargin )
+%TOSTRING - returns cell array of strings for many input types
+%   TOSTRING should provide data that is ready to input directly into
+%   legend and other functions that require special handling for each data
+%   type.
+%
+% SYNTAX:
+%   [ str_items ] = toString( items )
+%
+% Description:
+%   [ output_args ] = toString( items ) further description about 
+%        the use of the function can be added here.
+%
+% INPUTS:
+%   items - Description   
+%
+% OUTPUTS:
+%   str_items - Description
+%
+% SEE ALSO: NUM2STR, CELLSTR, CATEGORIES
+% 
+% Author:       nick roth
+% email:        nick.roth@nou-systems.com
+% Matlab ver.:  8.3.0.532 (R2014a)
+% Date:         20-Aug-2014
+% Update:
+
+if isnumeric(items)
+    items = num2str(items);
+    str_items = cellstr(items);
+elseif iscategorical(items)
+    str_items = categories(items);
+end
+      
+end
